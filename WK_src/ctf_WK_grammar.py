586e523e2190b23f3c46be85d3cf04f74b6dc775
honza <hhohha@seznam.cz>
2022-03-22 14:07:50 +0100

    .

--- WK_src/ctf_WK_grammar.py
+++ WK_src/ctf_WK_grammar.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
 
-#class cWordStatus:
-	#def __init__(self, word, upperStrLen, lowerStrLen):
-		#self.word = word
-		#self.upperStrLen = upperStrLen
-		#self.lowerStrLen = lowerStrLen
+class cWordStatus:
+	def __init__(self, word, upperStrLen, lowerStrLen, ntLen):
+		self.word = word
+		self.upperStrLen = upperStrLen
+		self.lowerStrLen = lowerStrLen
+		self.ntLen = ntLen
 
-#class cNonTerminal:
-	#def __init__(self, sign)
-		#self.sign = sign
+def tuplify(l):
+	if type(l) == list:
+		return tuple(tuplify(x) for x in l)
+	else:
+		return l
 
-#class cTerminal:
-	#def __init__(self, sign)
-		#self.sign = sign
+def wordToStr(word):
+	rs = []
+	for symbol in word:
+		if isinstance(symbol, list) or isinstance(symbol, tuple):
+			e1 = ''.join(symbol[0]) if len(symbol[0]) > 0 else 'λ'
+			e2 = ''.join(symbol[1]) if len(symbol[1]) > 0 else 'λ'
+			rs.append(e1 + '/' + e2)
+		else:
+			rs.append(symbol)
 
-#class cRule:
-	#def __init__(self, lhs, rhs):
-		#self.lhs = lhs
-		#self.rhs = rhs
-
+	return(f'{" ".join(rs)}')
 
 class cWK_CFG:
 	def __init__(self, nts, ts, startSymbol, rules, relation):
@@ -28,13 +33,13 @@ class cWK_CFG:
 		self.rules = rules
 		self.relation = relation
 
-		if not self.is_consistent():
-			raise ValueError
+		#if not self.is_consistent():
+			#raise ValueError
 
 		#TODO make rules compact: A -> (a lambda)(lambda a) == A -> (a a)
 		self.ruleDict = {}
 		for nt in self.nts:
-			ruleDict[nt] = []
+			self.ruleDict[nt] = []
 		for rule in rules:
 			self.ruleDict[rule[0]].append(rule[1])
 
@@ -62,74 +67,109 @@ class cWK_CFG:
 	def can_generate(self, upperStr):
 		#initStatus = cWordStatus([self.startSymbol], 0, 0)
 		#openQueue = [initStatus]
-		openQueue = [[self.startSymbol]]
+		openQueue = [tuple(self.startSymbol)]
+		openSet = set(tuple(self.startSymbol))
 		closedQueue = set()
+		cnt = 0
 
 		while openQueue:
+			if cnt == 15000:
+				print('taking too long')
+				return None
+			else:
+				cnt += 1
+			#print(f'cnt: {cnt}')
 			currentWord = openQueue.pop(0)
 			closedQueue.add(currentWord)
 
-			for nextWord in self.get_next_states(currentWord):
+			for nextWord in self.get_all_next_states(currentWord):
 				if self.is_result(nextWord, upperStr):
 					return True
-				if nextWord not in openQueue and nextWord not in closedQueue:
+				if nextWord not in openSet and nextWord not in closedQueue:
+				#if nextWord not in openQueue:
 					openQueue.append(nextWord)
+					openSet.add(nextWord)
 
 		return False
 
-	def is_result(word, goal):
-		return len(word) == 1 and word[0] == goal
+	def is_result(self, word, goal):
+		return len(word) == 1 and ''.join(word[0][0]) == goal
+
+	def is_rule_applicable(self, word, ntIdx, rule):
+		return True
 
 	def get_all_next_states(self, word):
+		#print(f'{wordToStr(word)}  --->  ')
 		for ntIdx, symbol in enumerate(word):
-			if not isinstance(symbol, list): # terminals are tuples so symbol is a non terminal
+			if not isinstance(symbol, list) and not isinstance(symbol, tuple): # terminals are tuples so symbol is a non terminal
 				for rule in self.ruleDict[symbol]:
-					newWord = self.apply_rule(word, ntIdx, rule)
-					if newWord:
-						yield newWord
+					if self.is_rule_applicable(word, ntIdx, rule):
+						yield self.apply_rule(word, ntIdx, rule)
+		#print('\n\n')
+
 
 	def apply_rule(self, word, ntIdx, rule):
-		isTerm = len(rule[1]) == 1 and isinstance(rule[1], list)   # rule right side is just a terminal
-		mergePrev = ntIdx > 0 and isinstance(word[ntIdx - 1], list)  # can we merge with the previous terminal
-		mergeNext = ntIdx < len(word) - 1 and isinstance(word[ntIdx + 1], list) # can we merge with the next terminal
+		#print(f'word: {wordToStr(word)}')
+		#print(f'ntIdx: {ntIdx}')
+		#print(rule)
+		#print(f'rule: {word[ntIdx] + " -> " + wordToStr(rule):20}\n')
+
+		isTerm = len(rule) == 1 and (isinstance(rule, list) or isinstance(rule, tuple))   # rule right side is just a terminal
+		mergePrev = ntIdx > 0 and (isinstance(word[ntIdx - 1], list) or isinstance(word[ntIdx - 1], tuple)) # can we merge with the previous terminal
+		mergeNext = ntIdx < len(word) - 1 and (isinstance(word[ntIdx + 1], list) or isinstance(word[ntIdx + 1], tuple))# can we merge with the next terminal
 
 		if isTerm:
 			if mergePrev and mergeNext:
-				mergedUpper = word[ntIdx - 1][0] + rule[1][0][0] + word[ntIdx + 1][0]
-				mergedLower = word[ntIdx - 1][1] + rule[1][0][1] + word[ntIdx + 1][1]
-				return word[:ntIdx - 1] + [(mergedUpper, mergedLower)] + word[ntIdx + 2:]
+				mergedUpper = word[ntIdx - 1][0] + rule[0][0] + word[ntIdx + 1][0]
+				mergedLower = word[ntIdx - 1][1] + rule[0][1] + word[ntIdx + 1][1]
+				return word[:ntIdx - 1] + tuple([(mergedUpper, mergedLower)]) + word[ntIdx + 2:]
 
 			elif mergePrev:
-				mergedUpper = word[ntIdx - 1][0] + rule[1][0][0]
-				mergedLower = word[ntIdx - 1][1] + rule[1][0][1]
-				return word[:ntIdx - 1] + [(mergedUpper, mergedLower)] + word[ntIdx + 1:]
+				mergedUpper = word[ntIdx - 1][0] + rule[0][0]
+				mergedLower = word[ntIdx - 1][1] + rule[0][1]
+				return word[:ntIdx - 1] + tuple([(mergedUpper, mergedLower)]) + word[ntIdx + 1:]
 
 			elif mergeNext:
-				mergedUpper = rule[1][0][0] + word[ntIdx + 1][0]
-				mergedLower = rule[1][0][1] + word[ntIdx + 1][1]
-				return word[:ntIdx] + [(mergedUpper, mergedLower)] + word[ntIdx + 2:]
+				mergedUpper = rule[0][0] + word[ntIdx + 1][0]
+				mergedLower = rule[0][1] + word[ntIdx + 1][1]
+				return word[:ntIdx] + tuple([(mergedUpper, mergedLower)]) + word[ntIdx + 2:]
 
 			else:
-				return word[:ntIdx] + [rule[1][0]] + word[ntIdx + 1:]
+				return word[:ntIdx] + tuple([rule[0]]) + word[ntIdx + 1:]
 
-		mergePrev = mergePrev and isinstance(rule[1][0], list)
-		mergeNext = mergeNext and isinstance(rule[1][-1], list)
+		mergePrev = mergePrev and (isinstance(rule[0], list) or isinstance(rule[0], tuple))
+		mergeNext = mergeNext and (isinstance(rule[-1], list) or isinstance(rule[-1], tuple))
 
 		if mergePrev and mergeNext:
-			mergedUpperPrev = word[ntIdx - 1][0] + rule[1][0][0]
-			mergedLowerPrev = word[ntIdx - 1][1] + rule[1][0][1]
-			mergedUpperNext = rule[1][-1][0] + word[ntIdx + 1][0]
-			mergedLowerNext = rule[1][-1][1] + word[ntIdx + 1][1]
-			return word[:ntIdx - 1] + [(mergedUpperPrev, mergedLowerPrev)] + rule[1][1:-1] + [(mergedUpperNext, mergedLowerNext)] + word[ntIdx + 2:]
+			mergedUpperPrev = word[ntIdx - 1][0] + rule[0][0]
+			mergedLowerPrev = word[ntIdx - 1][1] + rule[0][1]
+			mergedUpperNext = rule[-1][0] + word[ntIdx + 1][0]
+			mergedLowerNext = rule[-1][1] + word[ntIdx + 1][1]
+			return word[:ntIdx - 1] + tuple([(mergedUpperPrev, mergedLowerPrev)]) + rule[1:-1] + [(mergedUpperNext, mergedLowerNext)] + word[ntIdx + 2:]
 		elif mergePrev:
-			mergedUpperPrev = word[ntIdx - 1][0] + rule[1][0][0]
-			mergedLowerPrev = word[ntIdx - 1][1] + rule[1][0][1]
-			return word[:ntIdx - 1] + [(mergedUpperPrev, mergedLowerPrev)] + rule[1][1:] + word[ntIdx + 1:]
+			mergedUpperPrev = word[ntIdx - 1][0] + rule[0][0]
+			mergedLowerPrev = word[ntIdx - 1][1] + rule[0][1]
+			return word[:ntIdx - 1] + tuple([(mergedUpperPrev, mergedLowerPrev)]) + rule[1:] + word[ntIdx + 1:]
 		elif mergeNext:
-			mergedUpperNext = rule[1][-1][0] + word[ntIdx + 1][0]
-			mergedLowerNext = rule[1][-1][1] + word[ntIdx + 1][1]
-			return word[:ntIdx] + rule[1][:-1] + [(mergedUpperNext, mergedLowerNext)] + word[ntIdx + 2:]
+			mergedUpperNext = rule[-1][0] + word[ntIdx + 1][0]
+			mergedLowerNext = rule[-1][1] + word[ntIdx + 1][1]
+			return word[:ntIdx] + rule[:-1] + tuple([(mergedUpperNext, mergedLowerNext)]) + word[ntIdx + 2:]
 		else:
-			return word[:ntIdx] + rule[1] + word[ntIdx + 1:]
+			return word[:ntIdx] + rule + word[ntIdx + 1:]
 
 
+nts = ['A']
+ts = ['a']
+startSymbol = 'A'
+rules = [
+	('A', ('A', 'A')),
+	('A', tuplify([[['a'], ['a']]]))
+]
+relation = [('a', 'a')]
+g = cWK_CFG(nts, ts, startSymbol, rules, relation)
+
+res = g.can_generate('aaaaaa')
+print(f'RESULT: {res}')
+
+#for x in g.get_all_next_states([(['a', 'a'], ['a', 'a']), 'A']):
+	#print(f'-----> {wordToStr(x)}\n\n\n')
--- WK_src/ctf_WK_grammar_test.py
+++ WK_src/ctf_WK_grammar_test.py
@@ -1,28 +1,18 @@
 #!/usr/bin/python3
 
-from ctf_WK_grammar import cWK_CFG
+from ctf_WK_grammar import *
 
 RES_OK = '\033[92m' + 'OK' + '\x1b[0m'
 RES_FAILED = '\033[91m' + 'FAILED' + '\x1b[0m'
 
-def strWord(word):
-	rs = []
-	for symbol in word:
-		if isinstance(symbol, list) or isinstance(symbol, tuple):
-			e1 = ''.join(symbol[0]) if len(symbol[0]) > 0 else 'λ'
-			e2 = ''.join(symbol[1]) if len(symbol[1]) > 0 else 'λ'
-			rs.append(e1 + '/' + e2)
-		else:
-			rs.append(symbol)
-
-	return(f'{" ".join(rs)}')
 
 
-def runTest(cnt, word, ntIdx, rule, expected):
-	actual = strWord(cWK_CFG.apply_rule(None, word, ntIdx, rule))
+
+def runTest(cnt, word, ntIdx, ruleRhs, expected):
+	actual = wordToStr(cWK_CFG.apply_rule(None, word, ntIdx, rule))
 	status = RES_OK if actual == expected else RES_FAILED
 	#print(f'TEST {cnt:4}       {word:20} | {ntIdx:3} | {rule:20} | {expected:20} | {actual:20}         {status:10}')
-	print(f'| TEST {cnt:2}       |    {strWord(word):20}    |    {ntIdx:3}    |    {rule[0] + " -> " + strWord(rule[1]):20}    |    {expected:20}    |    {actual:20}    | {status:15}   |')
+	print(f'| TEST {cnt:2}       |    {wordToStr(word):20}    |    {ntIdx:3}    |    {word[ntIdx] + " -> " + wordToStr(rule):20}    |    {expected:20}    |    {actual:20}    | {status:15}   |')
 	print(hline)
 
 hline = '|---------------|----------------------------|-----------|----------------------------|----------------------------|--------------------------- |----------|'
@@ -33,7 +23,7 @@ print('|               |           WORD             |   NT IDX  |         RULE  
 print(hline)
 print(hline)
 
-rule = ('A', [[['a'], ['a']]])
+rule = [[['a'], ['a']]]
 runTest( 1, ['A'],                                         0, rule, 'a/a')
 runTest( 2, [[['a'], ['a']], 'A'],                         1, rule, 'aa/aa')
 runTest( 3, ['A', [['a'], ['a']]],                         0, rule, 'aa/aa')
@@ -42,7 +32,7 @@ runTest( 5, [[['a'], ['a']], 'A', 'B'],                    1, rule, 'aa/aa B')
 runTest( 6, ['B', 'A', [['b'], ['b']]],                    1, rule, 'B ab/ab')
 runTest( 7, ['A', 'A', 'B'],                               1, rule, 'A a/a B')
 
-rule = ('A', [[['a'], ['a']], 'B', [['c'], ['c']]])
+rule = [[['a'], ['a']], 'B', [['c'], ['c']]]
 runTest( 8, ['A', 'A', 'A'],                               1, rule, 'A a/a B c/c A')
 runTest( 9, ['A', 'A', 'A'],                               0, rule, 'a/a B c/c A A')
 runTest(10, ['A', 'A', [['a'], ['b']]],                    1, rule, 'A a/a B ca/cb')
@@ -50,6 +40,6 @@ runTest(11, [[['a'], ['b']], 'A'],                         1, rule, 'aa/ba B c/c
 runTest(12, [[['a'], ['b']], 'A', [['c'], ['d']], 'B'],    1, rule, 'aa/ba B cc/cd B')
 runTest(13, [[['a'], []], 'A'],                            1, rule, 'aa/a B c/c')
 
-rule = ('A', [[[], ['b']], 'A', [['a'], []]])
+rule = [[[], ['b']], 'A', [['a'], []]]
 runTest(14, [[['a'], ['b']], 'A', [['c'], ['d']]],         1, rule, 'a/bb A ac/d')
 runTest(15, [[['a'], ['a']], 'A', 'B'],                    1, rule, 'a/ab A a/λ B')
