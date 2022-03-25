6468cb2f487562b1cb28b512c1d301767917642f
honza <hhohha@seznam.cz>
2022-03-25 12:42:12 +0100

    .

--- WK_src/ctf_WK_grammar.py
+++ WK_src/ctf_WK_grammar.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
 
-from typing import Dict, List, Tuple, Set, Union, Optional
+from typing import Dict, List, Tuple, Set, Union, Optional, TypeVar
+
+DEBUG = 0
+def debug(s):
+	if DEBUG:
+		print(s)
 
 tNonTerm = str
 tTerm = str
 tTermLetter = Tuple[List[tTerm], List[tTerm]]
-tLetter = Union[tNonTerm, tTermLetter]
+#tLetter = Union[tNonTerm, tTermLetter]
+tLetter = TypeVar('tLetter', tNonTerm, tTermLetter)
 tWord = List[tLetter]
 tRelation = Tuple[tTerm, tTerm]
+
+def is_nonterm(letter: tLetter) -> bool:
+	return not isinstance(letter, tuple)
 
 class cWordStatus:
 	def __init__(self, word: tWord, upperStrLen: int, lowerStrLen: int, ntLen: int, parent: Optional['cWordStatus']) -> None:
@@ -32,7 +41,7 @@ class cRule:
 		for i in range(len(word) - 1):
 			syllable1 = word[i]
 			syllable2 = word[i+1]
-			if isinstance(syllable1, tuple) and isinstance(syllable2, tuple):
+			if not is_nonterm(syllable1) and not is_nonterm(syllable2):
 				newSyllable = (syllable1[0] + syllable2[0], syllable1[1] + syllable2[1])
 				word[i:i+2] = [newSyllable]
 		return word
@@ -45,7 +54,7 @@ class cRule:
 		self.lowerCnt = 0
 
 		for letter in rhs:
-			if isinstance(letter, tuple):
+			if not is_nonterm(letter):
 				self.upperCnt += len(letter[0])
 				self.lowerCnt += len(letter[1])
 			else:
@@ -54,7 +63,7 @@ class cRule:
 def wordToStr(word: tWord) -> str:
 	rs = []
 	for symbol in word:
-		if isinstance(symbol, list) or isinstance(symbol, tuple):
+		if not is_nonterm(symbol):
 			e1 = ''.join(symbol[0]) if len(symbol[0]) > 0 else 'λ'
 			e2 = ''.join(symbol[1]) if len(symbol[1]) > 0 else 'λ'
 			rs.append(e1 + '/' + e2)
@@ -69,16 +78,43 @@ class cWK_CFG:
 		self.ts = ts
 		self.startSymbol = startSymbol
 		self.rules = rules
-		self.relation = relation
+		self.relation = set(relation)
+		self.erasableNts: Set[tNonTerm] = set()
 
 		if not self.is_consistent():
 			raise ValueError
 
-		self.ruleDict: dict[tNonTerm, List[cRule]] = {}
+		self.ruleDict: Dict[tNonTerm, List[cRule]] = {}
 		for nt in self.nts:
 			self.ruleDict[nt] = []
 		for rule in rules:
 			self.ruleDict[rule.lhs].append(rule)
+
+		loop = True
+		while loop:
+			loop = False
+			for rule in self.rules:
+				if self.is_word_erasable(rule.rhs) and rule.lhs not in self.erasableNts:
+					loop = True
+					self.erasableNts.add(rule.lhs)
+
+		for rule in self.rules:
+			for letter in rule.rhs:
+				if is_nonterm(letter) and letter in self.erasableNts:
+					rule.ntCnt -= 1
+
+	def is_word_erasable(self, word: tWord) -> bool:
+		for letter in word:
+			if not is_nonterm(letter):
+				if letter == ([], []):
+					continue
+				else:
+					return False
+
+			if letter not in self.erasableNts:
+				return False
+
+		return True
 
 	def is_consistent(self) -> bool:
 		if self.startSymbol not in self.nts:
@@ -109,7 +145,7 @@ class cWK_CFG:
 	def printPath(self, wordStatus: cWordStatus) -> None:
 		currentWordStatus: Optional[cWordStatus] = wordStatus
 		while currentWordStatus:
-			print(f' >>> {wordToStr(currentWordStatus.word)}')
+			debug(f' >>> {wordToStr(currentWordStatus.word)}')
 			currentWordStatus = currentWordStatus.parent
 
 
@@ -122,16 +158,18 @@ class cWK_CFG:
 		cnt = 0
 
 		while openQueue:
-			if cnt == 2:
+			if cnt == 100000:
 				print('taking too long')
 				return None
 			else:
 				cnt += 1
-			print(f'cnt: {cnt}')
+			debug('\n--------------------------------------')
+			debug(f'cnt: {cnt} (O: {len(openSet)}, C: {len(closedSet)})')
+			debug('--------------------------------------')
 			currentWordStatus = openQueue.pop(0)
 			closedSet.add(currentWordStatus.hashNo)
 
-			for nextWordStatus in self.get_all_next_states(currentWordStatus, len(upperStr)):
+			for nextWordStatus in self.get_all_next_states(currentWordStatus, upperStr):
 				if self.is_result(nextWordStatus.word, upperStr):
 					self.printPath(nextWordStatus)
 					return True
@@ -142,76 +180,115 @@ class cWK_CFG:
 		return False
 
 	def is_result(self, word: tWord, goal: str) -> bool:
-		return len(word) == 1 and ''.join(word[0][0]) == goal
+		# word lenght must be 1
+		# word must be terminal
+		# upper and lower strands must have the same length
+		# upper and lower strands must fulfil compl. relation
+		# upper strand must equal goal string
 
-	def is_rule_applicable(self, wordStatus: cWordStatus, ntIdx: int, rule: cRule, maxLen: int):
-		termCnt = max(wordStatus.upperStrLen + rule.upperCnt, wordStatus.lowerStrLen + rule.lowerCnt)
-		return termCnt + wordStatus.ntLen + rule.ntCnt - 1 <= maxLen
+		if len(word) != 1:
+			return False
 
-	def get_all_next_states(self, wordStatus: cWordStatus, maxLen: int) -> List[cWordStatus]:
+		if isinstance(word[0], tNonTerm):
+			return False
+
+		if len(word[0][0]) != len(word[0][1]):
+			return False
+
+		for symbol1, symbol2 in zip(word[0][0], word[0][1]):
+			if (symbol1, symbol2) not in self.relation:
+				return False
+
+		if ''.join(word[0][0]) != goal:
+			return False
+
+		return True
+
+	def is_word_feasible(self, wordStatus: cWordStatus, goalStr: str):
+		longerStrand = max(wordStatus.upperStrLen, wordStatus.lowerStrLen)
+		shorterStrand = min(wordStatus.upperStrLen, wordStatus.lowerStrLen)
+
+		if longerStrand > len(goalStr) or shorterStrand + wordStatus.ntLen > len(goalStr):
+			debug(f'not feasible (getting too long)')
+			return False
+
+		word = wordStatus.word
+		if not is_nonterm(word[0]):
+			if not goalStr.startswith(''.join(word[0][0])):
+				debug('not feasible (doesn\'t match goal string)')
+				return False
+
+			shorter_len = min(len(word[0][0]), len(word[0][1]))
+			for idx in range(shorter_len):
+				if (word[0][0][idx], word[0][1][idx]) not in self.relation:
+					debug('not feasible (doesn\'t fulfil relation)')
+					return False
+		debug ('feasible')
+		return True
+
+	def get_all_next_states(self, wordStatus: cWordStatus, goalStr: str) -> List[cWordStatus]:
 		retLst: List[cWordStatus] = []
 		for ntIdx, symbol in enumerate(wordStatus.word):
-			if not isinstance(symbol, list) and not isinstance(symbol, tuple): # terminals are tuples so symbol is a non terminal
+			if is_nonterm(symbol):
 				for rule in self.ruleDict[symbol]:
-					if self.is_rule_applicable(wordStatus, ntIdx, rule, maxLen):
-						newWord = self.apply_rule(wordStatus.word, ntIdx, rule.rhs)
+					newWord = self.apply_rule(wordStatus.word, ntIdx, rule.rhs)
+					newWordStatus = cWordStatus(newWord, wordStatus.upperStrLen + rule.upperCnt, wordStatus.lowerStrLen + rule.lowerCnt, wordStatus.ntLen + rule.ntCnt - 1, wordStatus)
 
-						upperStrLen = wordStatus.upperStrLen + rule.upperCnt
-						lowerStrLen = wordStatus.lowerStrLen + rule.lowerCnt
-						ntLen = wordStatus.ntLen + rule.ntCnt - 1
-						retLst.append(cWordStatus(newWord, upperStrLen, lowerStrLen, ntLen, wordStatus))
-						#yield cWordStatus(newWord, 0, 0, 1)
+					if self.is_word_feasible(newWordStatus, goalStr):
+						retLst.append(newWordStatus)
 		return retLst
 
 
 	def apply_rule(self, word: tWord, ntIdx: int, ruleRhs: tWord) -> tWord:
-		print(f'word: {wordToStr(word)}')
-		print(f'ntIdx: {ntIdx}')
-		#print(ruleRhs)
-		print(f'rule: {word[ntIdx] + " -> " + wordToStr(ruleRhs):20}\n')
+		if DEBUG:
+			debug(f'\nword: {wordToStr(word)}')
+			debug(f'ntIdx: {ntIdx}')
+			debug(f'rule: {word[ntIdx] + " -> " + wordToStr(ruleRhs)}')
 
-		isTerm = len(ruleRhs) == 1 and isinstance(ruleRhs[0], list)   # rule right side is just a terminal
-		mergePrev = ntIdx > 0 and isinstance(word[ntIdx - 1], list) # can we merge with the previous terminal
-		mergeNext = ntIdx < len(word) - 1 and isinstance(word[ntIdx + 1], list) # can we merge with the next terminal
+		mergePrev = ntIdx > 0 and not is_nonterm(word[ntIdx - 1]) # can we merge with the previous terminal
+		mergeNext = ntIdx < len(word) - 1 and not is_nonterm(word[ntIdx + 1]) # can we merge with the next terminal
 
-		if isTerm:
+		if len(ruleRhs) == 1 and not is_nonterm(ruleRhs[0]):    # rule right side is just a terminal pair
 			if mergePrev and mergeNext:
 				mergedUpper = word[ntIdx - 1][0] + ruleRhs[0][0] + word[ntIdx + 1][0]
 				mergedLower = word[ntIdx - 1][1] + ruleRhs[0][1] + word[ntIdx + 1][1]
-				return word[:ntIdx - 1] + [(mergedUpper, mergedLower)] + word[ntIdx + 2:]
+				retval = word[:ntIdx - 1] + [(mergedUpper, mergedLower)] + word[ntIdx + 2:]
 
 			elif mergePrev:
 				mergedUpper = word[ntIdx - 1][0] + ruleRhs[0][0]
 				mergedLower = word[ntIdx - 1][1] + ruleRhs[0][1]
-				return word[:ntIdx - 1] + [(mergedUpper, mergedLower)] + word[ntIdx + 1:]
+				retval = word[:ntIdx - 1] + [(mergedUpper, mergedLower)] + word[ntIdx + 1:]
 
 			elif mergeNext:
 				mergedUpper = ruleRhs[0][0] + word[ntIdx + 1][0]
 				mergedLower = ruleRhs[0][1] + word[ntIdx + 1][1]
-				return word[:ntIdx] + [(mergedUpper, mergedLower)] + word[ntIdx + 2:]
+				retval = word[:ntIdx] + [(mergedUpper, mergedLower)] + word[ntIdx + 2:]
 
 			else:
-				return word[:ntIdx] + [ruleRhs[0]] + word[ntIdx + 1:]
-		mergePrev = mergePrev and  isinstance(ruleRhs[0], list)
-		mergeNext = mergeNext and isinstance(ruleRhs[-1], list)
-
-		if mergePrev and mergeNext:
-			mergedUpperPrev = word[ntIdx - 1][0] + ruleRhs[0][0]
-			mergedLowerPrev = word[ntIdx - 1][1] + ruleRhs[0][1]
-			mergedUpperNext = ruleRhs[-1][0] + word[ntIdx + 1][0]
-			mergedLowerNext = ruleRhs[-1][1] + word[ntIdx + 1][1]
-			return word[:ntIdx - 1] + [(mergedUpperPrev, mergedLowerPrev)] + ruleRhs[1:-1] + [(mergedUpperNext, mergedLowerNext)] + word[ntIdx + 2:]
-		elif mergePrev:
-			mergedUpperPrev = word[ntIdx - 1][0] + ruleRhs[0][0]
-			mergedLowerPrev = word[ntIdx - 1][1] + ruleRhs[0][1]
-			return word[:ntIdx - 1] + [(mergedUpperPrev, mergedLowerPrev)] + ruleRhs[1:] + word[ntIdx + 1:]
-		elif mergeNext:
-			mergedUpperNext = ruleRhs[-1][0] + word[ntIdx + 1][0]
-			mergedLowerNext = ruleRhs[-1][1] + word[ntIdx + 1][1]
-			return word[:ntIdx] + ruleRhs[:-1] + [(mergedUpperNext, mergedLowerNext)] + word[ntIdx + 2:]
+				retval = word[:ntIdx] + [ruleRhs[0]] + word[ntIdx + 1:]
 		else:
-			return word[:ntIdx] + ruleRhs + word[ntIdx + 1:]
+			mergePrev = mergePrev and not is_nonterm(ruleRhs[0])
+			mergeNext = mergeNext and not is_nonterm(ruleRhs[-1])
 
+			if ntIdx > 0 and mergePrev and mergeNext:
+				mergedUpperPrev = word[ntIdx - 1][0] + ruleRhs[0][0]
+				mergedLowerPrev = word[ntIdx - 1][1] + ruleRhs[0][1]
+				mergedUpperNext = ruleRhs[-1][0] + word[ntIdx + 1][0]
+				mergedLowerNext = ruleRhs[-1][1] + word[ntIdx + 1][1]
+				retval = word[:ntIdx - 1] + [(mergedUpperPrev, mergedLowerPrev)] + ruleRhs[1:-1] + [(mergedUpperNext, mergedLowerNext)] + word[ntIdx + 2:]
+			elif mergePrev:
+				mergedUpperPrev = word[ntIdx - 1][0] + ruleRhs[0][0]
+				mergedLowerPrev = word[ntIdx - 1][1] + ruleRhs[0][1]
+				retval = word[:ntIdx - 1] + [(mergedUpperPrev, mergedLowerPrev)] + ruleRhs[1:] + word[ntIdx + 1:]
+			elif mergeNext:
+				mergedUpperNext = ruleRhs[-1][0] + word[ntIdx + 1][0]
+				mergedLowerNext = ruleRhs[-1][1] + word[ntIdx + 1][1]
+				retval = word[:ntIdx] + ruleRhs[:-1] + [(mergedUpperNext, mergedLowerNext)] + word[ntIdx + 2:]
+			else:
+				retval = word[:ntIdx] + ruleRhs + word[ntIdx + 1:]
+
+		debug(f'result: {wordToStr(retval)}')
+		return retval
 
 #nts: List[tNonTerm] = ['A']
 #ts: List[tTerm] = ['a']
@@ -234,18 +311,16 @@ class cWK_CFG:
 #for x in g.get_all_next_states([(['a', 'a'], ['a', 'a']), 'A']):
 	#print(f'-----> {wordToStr(x)}\n\n\n')
 
-rulex: tWord = [[['a'], ['a']]]
+#rules: List[cRule] = [
+	#cRule('S', [(['a'], []), 'S']),
+	#cRule('S', [(['a'], []), 'A']),
+	#cRule('A', [(['b'], ['a']), 'A']),
+	#cRule('A', [(['b'], ['a']), 'B']),
+	#cRule('B', [([], ['b']), 'B']),
+	#cRule('B', [([], ['b'])])
+#]
 
-rules: List[cRule] = [
-	cRule('S', [(['a'], []), 'S']),
-	cRule('S', [(['a'], []), 'A']),
-	cRule('A', [(['b'], ['a']), 'A']),
-	cRule('A', [(['b'], ['a']), 'B']),
-	cRule('B', [([], ['b']), 'B']),
-	cRule('B', [([], ['b'])])
-]
+#g = cWK_CFG(['S', 'A', 'B'], ['a', 'b'], 'S', rules, [('a', 'a'), ('b', 'b')])
 
-g = cWK_CFG(['S', 'A', 'B'], ['a', 'b'], 'S', rules, [('a', 'a'), ('b', 'b')])
-
-res = g.can_generate('aaaaaaaaaaaaaaa')
-print(f'RESULT: {res}')
+#res = g.can_generate('aaaaaaaaaaabbbbbbbbbbb')
+#print(f'RESULT: {res}')
--- WK_src/ctf_WK_grammar_test.py
+++ WK_src/ctf_WK_grammar_test.py
@@ -5,14 +5,17 @@ from ctf_WK_grammar import *
 RES_OK = '\033[92m' + 'OK' + '\x1b[0m'
 RES_FAILED = '\033[91m' + 'FAILED' + '\x1b[0m'
 
+testNo = 1
 
-def runTest(cnt: int, word: tWord, ntIdx: int, ruleRhs: tWord, expected: str) -> None:
+def runTest(word: tWord, ntIdx: int, ruleRhs: tWord, expected: str) -> None:
+	global testNo
 	g = cWK_CFG(['A'], [], 'A' , [], [])
 	actual = wordToStr(g.apply_rule(word, ntIdx, rule))
 	status = RES_OK if actual == expected else RES_FAILED
 	#print(f'TEST {cnt:4}       {word:20} | {ntIdx:3} | {rule:20} | {expected:20} | {actual:20}         {status:10}')
-	print(f'| TEST {cnt:2}       |    {wordToStr(word):20}    |    {ntIdx:3}    |    {word[ntIdx] + " -> " + wordToStr(rule):20}    |    {expected:20}    |    {actual:20}    | {status:15}   |')
+	print(f'| TEST {testNo:2}       |    {wordToStr(word):20}    |    {ntIdx:3}    |    {word[ntIdx] + " -> " + wordToStr(rule):20}    |    {expected:20}    |    {actual:20}    | {status:15}   |')
 	print(hline)
+	testNo += 1
 
 hline = '|---------------|----------------------------|-----------|----------------------------|----------------------------|----------------------------|----------|'
 
@@ -22,63 +25,183 @@ print('|               |           WORD             |   NT IDX  |         RULE  
 print(hline)
 print(hline)
 
-rule: tWord = [[['a'], ['a']]]
-runTest( 1, ['A'],                                         0, rule, 'a/a')
-runTest( 2, [[['a'], ['a']], 'A'],                         1, rule, 'aa/aa')
-runTest( 3, ['A', [['a'], ['a']]],                         0, rule, 'aa/aa')
-runTest( 4, [[['a'], ['b']], 'A', [['c'], ['d']]],         1, rule, 'aac/bad')
-runTest( 5, [[['a'], ['a']], 'A', 'B'],                    1, rule, 'aa/aa B')
-runTest( 6, ['B', 'A', [['b'], ['b']]],                    1, rule, 'B ab/ab')
-runTest( 7, ['A', 'A', 'B'],                               1, rule, 'A a/a B')
+rule: tWord = [(['a'], ['a'])]
+runTest(['A'],                                         0, rule, 'a/a')
+runTest([(['a'], ['a']), 'A'],                         1, rule, 'aa/aa')
+runTest(['A', (['a'], ['a'])],                         0, rule, 'aa/aa')
+runTest([(['a'], ['b']), 'A', (['c'], ['d'])],         1, rule, 'aac/bad')
+runTest([(['a'], ['a']), 'A', 'B'],                    1, rule, 'aa/aa B')
+runTest(['B', 'A', (['b'], ['b'])],                    1, rule, 'B ab/ab')
+runTest(['A', 'A', 'B'],                               1, rule, 'A a/a B')
 
-rule = [[['a'], ['a']], 'B', [['c'], ['c']]]
-runTest( 8, ['A', 'A', 'A'],                               1, rule, 'A a/a B c/c A')
-runTest( 9, ['A', 'A', 'A'],                               0, rule, 'a/a B c/c A A')
-runTest(10, ['A', 'A', [['a'], ['b']]],                    1, rule, 'A a/a B ca/cb')
-runTest(11, [[['a'], ['b']], 'A'],                         1, rule, 'aa/ba B c/c')
-runTest(12, [[['a'], ['b']], 'A', [['c'], ['d']], 'B'],    1, rule, 'aa/ba B cc/cd B')
-runTest(13, [[['a'], []], 'A'],                            1, rule, 'aa/a B c/c')
+rule = [(['a'], ['a']), 'B', (['c'], ['c'])]
+runTest(['A', 'A', 'A'],                               1, rule, 'A a/a B c/c A')
+runTest(['A', 'A', 'A'],                               0, rule, 'a/a B c/c A A')
+runTest(['A', 'A', (['a'], ['b'])],                    1, rule, 'A a/a B ca/cb')
+runTest([(['a'], ['b']), 'A'],                         1, rule, 'aa/ba B c/c')
+runTest([(['a'], ['b']), 'A', (['c'], ['d']), 'B'],    1, rule, 'aa/ba B cc/cd B')
+runTest([(['a'], []), 'A'],                            1, rule, 'aa/a B c/c')
 
-rule = [[[], ['b']], 'A', [['a'], []]]
-runTest(14, [[['a'], ['b']], 'A', [['c'], ['d']]],         1, rule, 'a/bb A ac/d')
-runTest(15, [[['a'], ['a']], 'A', 'B'],                    1, rule, 'a/ab A a/λ B')
+rule = [([], ['b']), 'A', (['a'], [])]
+runTest([(['a'], ['b']), 'A', (['c'], ['d'])],         1, rule, 'a/bb A ac/d')
+runTest([(['a'], ['a']), 'A', 'B'],                    1, rule, 'a/ab A a/λ B')
 
+rule = [(['a'], []), 'S']
+runTest([(['a'], []), 'S'],                    1, rule, 'aa/λ S')
 
-rule = [[['a'], []], 'S']
-runTest(16, [[['a'], []], 'S'],                    1, rule, 'aa/λ S')
+testNo = 1
+def runTest2(grammar: cWK_CFG, inputStr: str, expected: bool):
+	global testNo
+	actual = grammar.can_generate(inputStr)
+	status = RES_OK if actual == expected else RES_FAILED
 
-
-
-#rules: List[cRule] = [
-	#cRule('S', [(['a'], []), 'S']),
-	#cRule('S', [(['a'], []), 'A']),
-	#cRule('A', [(['b'], ['a']), 'A']),
-	#cRule('A', [(['b'], ['a']), 'B']),
-	#cRule('B', [([], ['b']), 'B']),
-	#cRule('B', [([], ['b'])])
-#]
-
-#g = cWK_CFG(['S', 'A', 'B'], ['a', 'b'], 'S', rules, [('a', 'a'), ('b', 'b')])
-
-#def runTest2(cnt: int, grammar: cWK_CFG, inputStr: str, expected: bool):
-	#actual = grammar.can_generate(inputStr)
-	#status = RES_OK if actual == expected else RES_FAILED
-
-	#print(f'| TEST {cnt:2}       |    {inputStr:17}   |    {expected:6}    |    {actual:6}  | {status:15}  |')
+	print(f'| TEST {testNo:2}       | {g.desc:35} | {inputStr:40} |    {expected:6}    |    {actual:6}  | {status:15}  |')
 	#print(hline)
+	testNo += 1
 
 
-#hline = '|---------------|------------------------|--------------|------------|---------|'
-#print('\n\n\n')
-#print(hline)
-#print('|               |        STRING          |   EXPECTED   |   ACTUAL   | STATUS  |')
-#print(hline)
-#print(hline)
+hline = '|---------------|-------------------------------------|------------------------------------------|--------------|------------|---------|'
+print('\n\n\n')
+print(hline)
+print('|               |   GRAMMAR                           |        STRING                            |   EXPECTED   |   ACTUAL   | STATUS  |')
+print(hline)
+print(hline)
 
-#runTest2(1, g, 'ab', True)
-#runTest2(2, g, 'aaabbb', True)
-#runTest2(3, g, 'aaabb', False)
-#runTest2(4, g, 'abab', False)
-#runTest2(4, g, '', False)
-#runTest2(4, g, 'aabb', True)
-#runTest2(5, g, 'abc', False)
+############################ GRAMMAR 1:   a(aa)*      #####################################################################################
+
+rules: List[cRule] = [
+	cRule('A', ['A', 'A', 'A']),
+	cRule('A', [(['a'], ['a'])])
+]
+g = cWK_CFG(['A'], ['a'], 'A', rules, [('a', 'a')])
+g.desc = 'a(aa)*'
+
+runTest2(g, '', False)
+runTest2(g, 'a', True)
+runTest2(g, 'aa', False)
+runTest2(g, 'aaa', True)
+runTest2(g, 'aaaaaaaaaaa', True)
+runTest2(g, 'aaaaaaaaaaaa', False)
+
+############################ GRAMMAR 2:   a^n b^n (n>0)    ################################################################################
+rules  = [
+	cRule('S', [(['a'], []), 'S']),
+	cRule('S', [(['a'], []), 'A']),
+	cRule('A', [(['b'], ['a']), 'A']),
+	cRule('A', [(['b'], ['a']), 'B']),
+	cRule('B', [([], ['b']), 'B']),
+	cRule('B', [([], ['b'])])
+]
+
+g = cWK_CFG(['S', 'A', 'B'], ['a', 'b'], 'S', rules, [('a', 'a'), ('b', 'b')])
+g.desc = 'a^n b^n (n>0)'
+
+runTest2(g, 'ab', True)
+runTest2(g, 'aaabbb', True)
+runTest2(g, 'aaabb', False)
+runTest2(g, 'abab', False)
+runTest2(g, '', False)
+runTest2(g, 'aabb', True)
+runTest2(g, 'abc', False)
+
+############################ GRAMMAR 3:   r^n d^n u^n r^n    ##############################################################################
+rules: List[cRule] = [
+	cRule('S', [(['r'], []), 'S']),
+	cRule('S', [(['r'], []), 'A']),
+	cRule('A', [(['d'], ['r']), 'A']),
+	cRule('A', [(['d'], ['r']), 'B']),
+	cRule('B', [(['u'], ['d']), 'B']),
+	cRule('B', [(['u'], ['d']), 'C']),
+	cRule('C', [(['r'], ['u']), 'C']),
+	cRule('C', [(['r'], ['u']), 'D']),
+	cRule('D', [([], ['r']), 'D']),
+	cRule('D', [([], ['r'])])
+]
+
+g = cWK_CFG(['S', 'A', 'B', 'C', 'D'], ['r', 'd', 'u'], 'S', rules, [('r', 'r'), ('d', 'd'), ('u', 'u')])
+g.desc = 'r^n d^n u^n r^n'
+
+runTest2(g, 'rdur', True)
+runTest2(g, 'rrrrrrdddddduuuuuurrrrrr', True)
+runTest2(g, 'rrrrrrdddddduuuuuuurrrrrr', False)
+runTest2(g, 'rrrrrrddddduuuuuurrrrrr', False)
+
+############################ GRAMMAR 4:   a^n c^n b^n    ##################################################################################
+
+rules: List[cRule] = [
+	cRule('S', [(['a'], []), 'S', (['b'], [])]),
+	cRule('S', [(['a'], []), 'A', (['b'], [])]),
+	cRule('A', [(['c'], ['a']), 'A']),
+	cRule('A', [([], ['c']), 'B', ([], ['b'])]),
+	cRule('B', [([], ['c']), 'B', ([], ['b'])]),
+	cRule('B', [([], [])])
+]
+
+g = cWK_CFG(['S', 'A', 'B'], ['a', 'b', 'c'], 'S', rules, [('a', 'a'), ('b', 'b'), ('c', 'c')])
+g.desc = 'a^n c^n b^n'
+
+runTest2(g, 'aaaaaaaaaaaaccccccccccccbbbbbbbbbbbb', True)
+runTest2(g, 'aaaaaaaaaaaccccccccccccbbbbbbbbbbbb', False)
+
+############################ GRAMMAR 5:   a^n b^m c^n d^m     ##############################################################################
+
+rules: List[cRule] = [
+	cRule('S', [(['a'], []), 'S']),
+	cRule('S', [(['a'], []), 'A']),
+	cRule('A', [(['b'], []), 'A']),
+	cRule('A', [(['b'], []), 'B']),
+	cRule('B', [(['c'], ['a']), 'B']),
+	cRule('B', [(['c'], ['a']), 'C']),
+	cRule('C', [(['d'], ['b']), 'C']),
+	cRule('C', [(['d'], ['b']), 'D']),
+	cRule('D', [([], ['c']), 'D']),
+	cRule('D', [([], ['d']), 'D']),
+	cRule('D', [([], [])])
+]
+
+g = cWK_CFG(['S', 'A', 'B', 'C', 'D'], ['a', 'b', 'c', 'd'], 'S', rules, [('a', 'a'), ('b', 'b'), ('c', 'c'), ('d', 'd')])
+g.desc = 'a^n b^m c^n d^m'
+
+runTest2(g, 'aaaabbbbbbbccccddddddd', True)
+runTest2(g, 'aaaabbbbbbbccccdddddd', False)
+
+############################ GRAMMAR 6:   wcw where w in {a,b }*     ######################################################################
+
+rules: List[cRule] = [
+	cRule('S', [(['a'], []), 'S']),
+	cRule('S', [(['b'], []), 'S']),
+	cRule('S', [(['c'], []), 'A']),
+	cRule('A', [(['a'], ['a']), 'A']),
+	cRule('A', [(['b'], ['b']), 'A']),
+	cRule('A', [([], ['c']), 'B']),
+	cRule('B', [([], ['a']), 'B']),
+	cRule('B', [([], ['b']), 'B']),
+	cRule('B', [([], [])]),
+]
+
+
+
+g = cWK_CFG(['S', 'A', 'B', 'C'], ['a', 'b', 'c'], 'S', rules, [('a', 'a'), ('b', 'b'), ('c', 'c')])
+g.desc = 'wcw where w in {a,b }*'
+
+runTest2(g, 'abbabacabbaba', True)
+runTest2(g, 'abbabacababa', False)
+
+############################ GRAMMAR 6:   a^n b^m a^n where 2n <= m <= 3n   ###############################################################
+rules: List[cRule] = [
+	cRule('S', [(['a'], []), 'S', (['a'], ['a'])]),
+	cRule('S', [(['a'], []), 'A', (['a'], ['a'])]),
+	cRule('A', [(['b', 'b'], ['a']), 'A']),
+	cRule('A', [(['b', 'b', 'b'], ['a']), 'A']),
+	cRule('A', [([], ['b']), 'B']),
+	cRule('B', [([], ['b']), 'B']),
+	cRule('B', [([], [])])
+]
+
+g = cWK_CFG(['S', 'A', 'B'], ['a', 'b'], 'S', rules, [('a', 'a'), ('b', 'b')])
+g.desc = 'a^n b^m a^n where 2n <= m <= 3n'
+
+for m, n in [(m, n) for m in range(1, 7) for n in range (1, 7)]:
+	runTest2(g, 'a'*n + 'b'*m + 'a'*n, 2*n <= m and m <= 3*n)
+
+print(hline)
